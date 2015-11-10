edd320da2233b366c8bd61e0585014089d469f96
Neil <rlaborce@users.noreply.github.com>
2015-11-09 22:06:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	
+  $(window).scroll(function () { 
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 200) {
+      $('#nav').fadeIn();
+	  $('.go-up').fadeIn();
+	  $('.go-down').fadeOut();
+
+    }
+    if ($(window).scrollTop() < 200) {
+      $('#nav').fadeOut();
+	  $('.go-up').fadeOut();
+	  $('.go-down').fadeIn();
+    }
+  });	
+
+  //Taken from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+ 
+  
+});
